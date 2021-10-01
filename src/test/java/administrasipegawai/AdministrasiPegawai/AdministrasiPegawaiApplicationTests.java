35ffdb772018cf737f3a17a094132a6cf244b40f
zikri <90746878+zdevaria@users.noreply.github.com>
2021-10-01 15:35:36 +0700

    refactor package name for test

--- src/test/java/administrasipegawai/AdministrasiPegawai/AdministrasiPegawaiApplicationTests.java
+++ src/test/java/administrasipegawai/AdministrasiPegawai/AdministrasiPegawaiApplicationTests.java
@@ -1,5 +1,6 @@
-package com.administrasipegawai.AdministrasiPegawai;
+package administrasipegawai.AdministrasiPegawai;
 
+import AdministrasiPegawai.AdministrasiPegawaiApplication;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
